f474eff317b1be1688491a676d7c1cef52e3b6c5
sarthak vaidya <sarthakvaidya0@gmail.com>
2023-01-10 11:38:22 +0530

    route level middleware

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require("express");
-
+const route = express.Router();
 const app = express();
 
-app.use((req, res, next) => {
+// app.use((req, res, next) => {
+//   if (!req.query.age) {
+//     res.send("Please provide age");
+//   } else if (req.query.age < 18) {
+//     res.send("You can not access this page");
+//   } else {
+//     next();
+//   }
+// });
+
+const reqFilter = (req, res, next) => {
   if (!req.query.age) {
     res.send("Please provide age");
   } else if (req.query.age < 18) {
@@ -10,10 +20,26 @@ app.use((req, res, next) => {
   } else {
     next();
   }
-});
+};
+
+route.use(reqFilter);
 
 app.get("/", (req, res) => {
-  res.write("<h1>Welcome to node middleware</h1>");
+  res.send("<h1>Welcome to node middleware</h1>");
 });
+
+route.get("/about", (req, res) => {
+  res.send("<h1>Welcome to About Page</h1>");
+});
+
+route.get("/contact", reqFilter, (req, res) => {
+  res.send("<h1>Welcome to Contact Page</h1>");
+});
+
+app.get("/help", (req, res) => {
+  res.send("<h1>Welcome to Help Page</h1>");
+});
+
+app.use("/", route);
 
 app.listen(5000);
